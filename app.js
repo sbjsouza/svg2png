9ef6d2622d76d4331836063e6d2341b1422a4509
Samuel Souza <sbjs@cin.ufpe.br>
2021-02-15 16:46:21 -0300

    Add dynamic port for deploy

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const svg2png = require("svg2png");
 
 var app = express();
 
-app.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 app.get("/", (req, res, next) => {
